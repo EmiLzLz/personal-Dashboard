ff45f3db4f0e433352896b6860c641eb68e698cc
Emiliano Lopez <emiliano.ll289@gmail.com>
2025-07-05 18:22:24 -0600

    Creacion del componente summaryCard e integracion en Dashboard. Instalacion de la libreria de iconos lucide-react

---
+++ src/components/SummaryCard.jsx
@@ -0,0 +1,41 @@
+import useApi from "../hooks/useAPI";
+
+function SummaryCard({ title, icon: Icon, endpoint }) {
+  const { data, loading, error } = useApi(endpoint);
+  const totalData = data.length;
+
+  if (loading)
+    return <p className="text-center text-gray-400 mt-8">Cargando datos...</p>;
+  if (error)
+    return (
+      <p className="text-center text-red-500 mt-8">
+        Error al cargar los datos.
+      </p>
+    );
+
+  return (
+    <div className="relative">
+      {/* Icono sobresaliendo (float) en esquina superior izquierda */}
+      <div className="absolute -top-2 left-6 bg-pink-500 rounded-xl p-4 shadow-xl drop-shadow-lg z-10">
+        <Icon className="icon w-6 h-6 text-white" />
+      </div>
+
+      <div className="summary-card-container w-full sm:w-80 hover-effect relative p-6 pt-4 bg-white rounded-xl shadow-sm flex flex-col mt-4">
+        {/* Contenedor para título y número, alineado a la derecha */}
+        <div className="ml-auto text-right mt-4">
+          <p className="title text-sm font-medium text-gray-500 mb-2">
+            {title}
+          </p>
+          <div className="total text-4xl font-bold text-gray-800 mb-2">
+            {totalData}
+          </div>
+          <div className="text-sm text-green-600 font-medium border-t-2 w-full">
+            recently updated
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SummaryCard;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,11 +3,39 @@ import StatsLineChart from "../components/charts/LineChart";
 import StatsBarChart from "../components/charts/BarChart";
 import StatsPieChart from "../components/charts/PieChart";
 import CardSlider from "../components/StatsSlider";
+import SummaryCard from "../components/SummaryCard";
+import { FileText, MessageCircle, Image, Camera, Users } from "lucide-react";
 
 function Dashboard() {
   return (
     <div className="stats">
-      <div className="stats-squares"></div>
+      <div className="stats-squares grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6 w-full">
+        <SummaryCard
+          title={"POSTS"}
+          icon={FileText}
+          endpoint={"https://jsonplaceholder.typicode.com/posts"}
+        />
+        <SummaryCard
+          title={"COMMENTS"}
+          icon={MessageCircle}
+          endpoint={"https://jsonplaceholder.typicode.com/comments"}
+        />
+        <SummaryCard
+          title={"ALBUMS"}
+          icon={Image}
+          endpoint={"https://jsonplaceholder.typicode.com/albums"}
+        />
+        <SummaryCard
+          title={"PHOTOS"}
+          icon={Camera}
+          endpoint={"https://jsonplaceholder.typicode.com/photos"}
+        />
+        <SummaryCard
+          title={"USERS"}
+          icon={Users}
+          endpoint={"https://jsonplaceholder.typicode.com/users"}
+        />
+      </div>
       <div className="stats-graphs">
         <StatsLineChart />
         <StatsBarChart />
