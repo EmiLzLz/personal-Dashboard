c21e67b666c730d8aeb31eaa524b06baa7893761
Emiliano Lopez <emiliano.ll289@gmail.com>
2025-07-16 19:42:24 -0600

    Testing useLocalStorage custom hook

--- src/tests/hooks/useLocalSotrage.test.js
+++ src/tests/hooks/useLocalSotrage.test.js
@@ -1,6 +1,13 @@
+import { renderHook } from "@testing-library/react"
+import { describe, expect, it } from "vitest"
+import useLocalStorage from "../../hooks/useLocalStorage";
+
 describe('useLocalStorage', () => {
   it('debería devolver el valor inicial si no hay nada guardado', () => {
     // ...
+     const [value, setValues] = useLocalStorage("value", []);
+     const result = renderHook(() => value)
+     expect(result.current.value).toBe("")
   })
 
   it('debería guardar el valor actualizado en localStorage', () => {
