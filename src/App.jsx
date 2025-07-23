daea0d410f1fefbcbd5c8d94351fe6ffee68acbf
Emiliano Lopez <emiliano.ll289@gmail.com>
2025-07-23 11:20:11 -0600

    fix: configure routing with basename for github pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
@@ -13,19 +13,17 @@ import Login from "./pages/Login";
 function App() {
   useTheme();
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="create-note" element={<CreateNotes />} />
-          <Route path="notes" element={<Notes />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="login" element={<Login />} />
-          <Route path="signup" element={<Signup />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="create-note" element={<CreateNotes />} />
+        <Route path="notes" element={<Notes />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Toaster } from 'sonner'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import { Toaster } from 'sonner';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <Toaster/>
+    <BrowserRouter basename="/personal-Dashboard">
+      <App />
+      <Toaster />
+    </BrowserRouter>
   </StrictMode>
-)
+);
